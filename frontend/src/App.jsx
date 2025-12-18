775c002f3b7b4fea44a071793331b6dbbb435488
yesuraju1719 <garnepudiraju0@gmail.com>
2025-12-18 13:39:24 +0530

    Started SignUp Page Design and interactively designed Sign up Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Auth from './pages/Auth'
+import SignUpComp from './components/SignUpComp'
 import "./App.css"
 function App() {
 
   return (
     <>
       <div className="">
-        <Auth/>
+      <SignUpComp/>
       </div>
     </>
   )
